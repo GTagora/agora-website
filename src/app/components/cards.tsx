24f95a110d30d9b5ee2f9777e1a22736fe7ad548
ohu9 <oliviah221@gmail.com>
2025-04-16 17:15:16 -0400

    article card styles

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -21,14 +21,15 @@ export function ArticleCard({ article }: ArticleCard) {
     const link = article.link;
     return (
         <Link href={link}>
-        <div className={styles.card}>
-            <div className={styles.text}>
-                <h2>{title}</h2>
-                <h3>{genre}</h3>
+        <div className="w-[87vw] h-[210px] p-[30px] bg-white rounded-lg shadow-lg flex flex-row justify-between gap-6
+                        md:w-[40vw]">
+            <div className="overflow-ellipsis">
+                <h2 className="font-ebgaramond font-bold text-xl line-clamp-2 leading-6">{title}</h2>
+                <h3 className="font-sans text-xs opacity-60 my-2">{genre}</h3>
                 {/* <p className={styles.preview} dangerouslySetInnerHTML={{ __html: text.replace(/\\n/g, '\n')}}></p> */}
             </div>
             <Image 
-                className={styles.cardImage}
+                className="w-56 object-cover"
                 src={image ? image : '/test.jpg'}
                 width={400}
                 height={400}
