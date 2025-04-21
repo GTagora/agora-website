e45513d78917ccb8e8c894a197c391b7b89b780e
ohu9 <oliviah221@gmail.com>
2025-04-21 10:59:38 -0400

    home page logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-start p-24">
       
+      <div className="flex flex-row justify-center items-center gap-5">
+        <Image
+          className="w-16 object-cover"
+          src='/logo-black.png'
+          width={400}
+          height={400}
+          alt='article image'/>
+        <p className="font-playfairsc font-bold text-black text-[50pt] tracking-tighter">AGORA</p>
+      </div>
     </main>
   );
 }
