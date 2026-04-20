6086a570d8230cce98ba1542038071d283357470
ohu9 <oliviah221@gmail.com>
2026-04-20 13:41:59 -0400

    Update article tempate

--- src/app/(pages)/(issues)/26s/id-article-name/page.tsx
+++ src/app/(pages)/(issues)/26s/id-article-name/page.tsx
@@ -1,37 +1,41 @@
-import styles from './page.module.css'
-import Image from 'next/image'
-import Footer from '@/app/components/footer'
-
+import Image from "next/image";
+import Footer from "@/app/components/footer";
 
 export default async function Article() {
-    const author = 'author name';
-    const credit = 'Photo by someone';
-    const title = 'Article 1';
-    const date = 'Month 01, Year';
+  const author = "Author Name";
+  const credit = "Photo by Name";
+  const title = "Title";
+  const date = "May 2, 2026";
 
-    return (
-        <div>
-            <div className={styles.main}>
-                <div className={styles.container}>
-                    {/* <iframe className={styles.video} src='VIDEO LINK HERE'/> */}
-                    <Image 
-                    className={styles.image}
-                    src='/issues/issue/test.png'
-                    width={600}
-                    height={600}
-                    alt='article image'/>
-                    <p className = {styles.credit}>{credit}</p>
-                    <h1 className={styles.title}>{title}</h1>
-                    <h2 className={styles.author}>BY {author.toUpperCase()}</h2>
-                    <h3 className={styles.date}>{date}</h3>
-                    <div className={styles.text}>
-                        {/* REPLACE TEXT HERE */}
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </div>
-                </div>
-            </div>
-            <div className={styles.footer}>
-                <Footer />
-            </div>
+  return (
+    <div>
+      <div className="flex flex-col items-start md:pt-[140px] px-[10%] pb-60 min-h-screen bg-[var(--bg-color)] relative md:px-[20vw] md:py-[140px]">
+        <div className="mt-[100px] w-full md:mt-0">
+          <Image
+            className="w-full h-auto object-cover md:object-contain my-5 md:my-8 md:max-h-[70vh]"
+            src="/issues/26s/test.png"
+            width={600}
+            height={600}
+            alt="article image"
+          />
+          <p className="text-[9pt] font-sans tracking-wide text-center opacity-70 mb-10">
+            {credit}
+          </p>
+          <h1 className="font-playfair font-medium text-[30pt] md:text-[40pt] my-5 md:my-10">
+            {title}
+          </h1>
+          <h2 className="font-cormorant font-semibold text-xl mb-1 uppercase">
+            BY {author}
+          </h2>
+          <h3 className="font-normal text-md mb-8 text-neutral-500">{date}</h3>
+          <div className="font-serif text-sm md:text-lg leading-[200%] whitespace-pre-line">
+            {/*ARTICLE TEXT HERE */}
+          </div>
         </div>
-)};
+      </div>
+      <div className="w-full">
+        <Footer />
+      </div>
+    </div>
+  );
+}
