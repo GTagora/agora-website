50cdfe3a36de9f68bdee6013158a428180731cc8
Daniel Durling <dan@dgd.sh>
2026-04-20 09:12:03 -0400

    f25-17 addition: added 'Psalm 8';

--- src/app/(pages)/(issues)/25f/17-psalm-8/page.tsx
+++ src/app/(pages)/(issues)/25f/17-psalm-8/page.tsx
@@ -4,29 +4,73 @@ import Footer from '@/app/components/footer'
 
 
 export default async function Article() {
-    const author = 'author name';
-    const credit = 'Photo by someone';
-    const title = 'Article 1';
-    const date = 'Month 01, Year';
+    const author = 'Chase Whitener';
+    const credit = 'Photo by Joshua Chung';
+    const title = 'Psalm 8';
+    const date = 'December 6, 2025';
 
     return (
         <div>
             <div className={styles.main}>
                 <div className={styles.container}>
                     {/* <iframe className={styles.video} src='VIDEO LINK HERE'/> */}
-                    <Image 
-                    className={styles.image}
-                    src='/issues/issue/test.png'
-                    width={600}
-                    height={600}
-                    alt='article image'/>
-                    <p className = {styles.credit}>{credit}</p>
+                    <Image
+                        className={styles.image}
+                        src='/issues/25f/psalm-8.webp'
+                        width={600}
+                        height={600}
+                        alt='article image' />
+                    <p className={styles.credit}>{credit}</p>
                     <h1 className={styles.title}>{title}</h1>
                     <h2 className={styles.author}>BY {author.toUpperCase()}</h2>
                     <h3 className={styles.date}>{date}</h3>
                     <div className={styles.text}>
-                        {/* REPLACE TEXT HERE */}
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <p>The skies above are depths untold</p>
+                        <p>A deep stretching beyond the deep.</p>
+                        <p>Though speck am I among the stars,</p>
+                        <p>All worlds were yet made for me.</p>
+
+                        <br />
+
+                        <p>Let stars collide and nebulas commence</p>
+                        <p>The symphony longer than life.</p>
+                        <p>Though I— merely a breath for them</p>
+                        <p>Shall live to see them die!</p>
+
+                        <br />
+
+                        <p>Of all that lies beyond the sun</p>
+                        <p>Beyond heaven and what lies</p>
+                        <p>Our Lord decendeth not to come</p>
+                        <p>To worlds other than mine!</p>
+
+                        <br />
+
+                        <p>Though we the chosen race of God</p>
+                        <p>Call not creation’s center home,</p>
+                        <p>Of no other people doth Heaven say</p>
+                        <p>“For God so loved the world”</p>
+
+                        <br />
+
+                        <p>Though dark the deeds of men doth reign</p>
+                        <p>And scarcely beats the heart of love</p>
+                        <p>Of all mysteries most profound is this</p>
+                        <p>That Father’s love abidest still</p>
+
+                        <br />
+
+                        <p>Son of dust, cease not to see</p>
+                        <p>The relevance of thy relevance forever.</p>
+                        <p>Though short dark days ere dust bound fate</p>
+                        <p>Sowed dust shall reap Heaven’s man!</p>
+
+                        <br />
+
+                        <p>And raise an anthem older than time</p>
+                        <p>Deeper than that far reaching deep.</p>
+                        <p>Let all that hath breath declare</p>
+                        <p>Blessed be He that choseth me!</p>
                     </div>
                 </div>
             </div>
@@ -34,4 +78,5 @@ export default async function Article() {
                 <Footer />
             </div>
         </div>
-)};
+    )
+};
